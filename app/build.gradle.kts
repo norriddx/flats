3f457887b137f7f4cd3c0d366dc68334b2cc9366
irina <norriddx@gmail.com>
2026-03-14 15:50:40 +0400

    add permissions, serialization, supabase client

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,7 @@ val localProperties = Properties().apply {
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization")
 }
 
 android {
@@ -64,6 +65,7 @@ dependencies {
     implementation("io.github.jan-tennert.supabase:auth-kt")
     // Ktor
     implementation("io.ktor:ktor-client-okhttp:3.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/flats/data/SupabaseClient.kt
@@ -0,0 +1,16 @@
+package com.example.flats.data
+
+import com.example.flats.BuildConfig
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+
+object SupabaseClient {
+    val client = createSupabaseClient(
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_KEY
+    ) {
+        install(Auth)
+        install(Postgrest)
+    }
+}
